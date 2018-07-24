fd6e8d8610e2b3b54bf59ae14b1e52fdcbfa0b22
José Alberto <josealbertoandujar@gmail.com>
2018-07-24 12:04:32 +0200

    Add first template Web client

---
+++ web/js/app.js
@@ -0,0 +1,19 @@
+function moveUp(){
+    console.log('Move up');
+}
+
+function moveDown(){
+    console.log('Move Down');
+}
+
+function moveLeft(){
+    console.log('Move Left');
+}
+
+function moveRight(){
+    console.log('Move Right');
+}
+
+function sendSpeech(){
+    console.log('Send Speech ',  document.getElementById("speech").value);
+}
